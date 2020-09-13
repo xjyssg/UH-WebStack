c052a08de6e429a6d4f1b066411abc93c650c6b5
xjyssg <jiayuexue30@gmail.com>
2020-09-13 20:33:31 +0800

    1.11

--- part 1/unicafe/index.js
+++ part 1/unicafe/index.js
@@ -13,16 +13,59 @@ const Button = ({handler, text}) => (
   </button>
 )
 
-const Statistic = ({name, number}) => (
-  <p>{name} {number}</p>
-)
 
+const Table = ({goodPart, neutralPart, badPart}) => {
+  // let average = 
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{goodPart.name}</td>
+          <td>{goodPart.number}</td>
+        </tr>
+        <tr>
+          <td>{neutralPart.name}</td>
+          <td>{neutralPart.number}</td>
+        </tr>
+        <tr>
+          <td>{badPart.name}</td>
+          <td>{badPart.number}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{goodPart.number + neutralPart.number + badPart.number}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{String((goodPart.number - badPart.number) / (goodPart.number + neutralPart.number + badPart.number))}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{String((goodPart.number) / (goodPart.number + neutralPart.number + badPart.number) * 100)}%</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const goodPart = {
+    name: "good",
+    number: good
+  }
+  const neutralPart = {
+    name: "neutral",
+    number: neutral
+  }
+  const badPart = {
+    name: "bad",
+    number: bad
+  }
 
 
   return (
@@ -32,12 +75,7 @@ const App = () => {
       <Button handler={() => setNeutral(neutral+1)} text="neutral" />
       <Button handler={() => setBad(bad+1)} text="bad" />
       <Title text="statistics" />
-      <Statistic name="good" number={good} />
-      <Statistic name="neutral" number={neutral} />
-      <Statistic name="bad" number={bad} />
-      <Statistic name="all" number={good + neutral + bad} />
-      <Statistic name="average" number={(good - bad) / (good + neutral + bad)} />
-      <Statistic name="positive" number={((good) / (good + neutral + bad) * 100) + "%"} />
+      <Table goodPart={goodPart} neutralPart={neutralPart} badPart={badPart} />
     </div>
   )
 }
