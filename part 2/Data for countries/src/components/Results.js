a65f60b16aa67b649707a2eb90a34fc6262a4510
xjyssg <jiayuexue30@gmail.com>
2020-09-15 22:38:35 +0800

    2.14

--- part 2/Data for countries/src/components/Results.js
+++ part 2/Data for countries/src/components/Results.js
@@ -19,7 +19,6 @@ const Details = ({country}) => (
 
 const ShowMore = ({show, tmpResults}) => {
   let finalResults = tmpResults.filter(record => show.indexOf(record.name) !== -1)
-  console.log(finalResults.length)
   return (
     <div>
       {finalResults.map(result => <Details key={result.name} country={result} />)}
@@ -27,9 +26,43 @@ const ShowMore = ({show, tmpResults}) => {
   )
 }
 
+const WeatherDetails = ({info}) => {
+  console.log("@@@", info)
+  if (JSON.stringify(info) === '{}') {
+    return <p></p>
+  } else {
+    return (
+      <div>
+      <h2>Weather in {info.location.name}</h2>
+      <div>temperature: {info.current.temperature} Celsius</div>
+      <img src={info.current.weather_icons[0]} width="100"></img>
+      <div>wind: {info.current.wind_speed}mph direction {info.current.wind_dir}</div>
+      </div>
+    )
+  }
+}
+
+const Weather = ({name}) => {
+  const [ info, setInfo ] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${name}`)
+      .then(response => {
+        console.log(response.data)
+        setInfo(response.data)
+      })
+  }, [])
+  
+  return <WeatherDetails info={info} />
+
+
+}
+
 const Results = ({filter, records}) => {
 
   const [ show, setShow ] = useState([])
+  const [ weather, setWeather ] = useState([])
   
 
   const handlerShow = (name) => {
@@ -38,7 +71,6 @@ const Results = ({filter, records}) => {
     }
     return handler
   }
-
 
   if (filter === '') {
     return 'Input something'
@@ -63,7 +95,10 @@ const Results = ({filter, records}) => {
       )
     } else if (tmpResults.length === 1){
       return (
-        <Details country={tmpResults[0]} />
+        <div>
+          <Details country={tmpResults[0]} />
+          <Weather name={tmpResults[0].name} />
+        </div>
       )
     } else {
         return 'Nothing found'
